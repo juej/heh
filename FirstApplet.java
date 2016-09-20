d71f655c39ee0404f86a6a9bbdd2ef468777177c
inegve <inegve@list.ru>
2016-09-20 23:51:12 +0300

    k

---
+++ FirstApplet.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Eugeny on 16.09.2016.
+ */
+
+import javax.swing.*;
+import java.applet.*;
+import java.awt.*;
+
+public class FirstApplet extends Applet {
+    Image img;
+    String myGroup;
+
+    public void init() {
+//        img = getImage(getCodeBase(), "H:\\lab0\\SimpleApplet\\Home\\FirstApplet\\cupofjava.gif");
+        img = getImage(getCodeBase(), "ja.jpg");
+        myGroup = getParameter("GROUP");
+        setBackground(Color.darkGray);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.PLAIN, 16));
+        g.drawRect(0, 0, getSize().width - 1, getSize().height - 1);
+        g.drawImage(img, 10, 100, this);
+        g.drawString("Honorable students of group " + myGroup + "!", 350, 40);
+        g.drawString("Congratulate you on the beggining of the course", 325, 60);
+        g.drawString("\"Programming in Java\"", 400, 80);
+    }
+}
---
+++ compile.bat
@@ -0,0 +1,4 @@
+set JAVA_PATH="C:\Program Files (x86)\Java\jdk1.8.0_91"
+set PATH=%PATH%;"C:\Program Files (x86)\Java\jdk1.8.0_91\bin"
+javac -deprecation FirstApplet.java
+pause
